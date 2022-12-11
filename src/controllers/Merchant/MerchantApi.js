f051ac1e9a424bc9b45e35646beefb851adc2288
mpawas <pawas@akdroid.com>
2022-12-11 15:12:32 +0545

    api issue fixed

--- src/controllers/Merchant/MerchantApi.js
+++ src/controllers/Merchant/MerchantApi.js
@@ -8,7 +8,7 @@ const { dataSuccess, serverError, validationError } = require("../../utilities/r
 exports.ApiController = async (req, res) => {
     try {
         const body = req.body
-        const merchant = await Merchant.findOne({ where: { merchant_code: body.MC, secret_key :body.SC } })
+        const merchant = await Merchant.findOne({ where: { merchant_code: body.MC, secret_key: body.SK }, include: [{ model: User }] })
         const user = await User.findOrCreate({ where: { phone: body.UP }, defaults: { phone: body.UP, uid: generateUId(), id: generateId() } })
         const pointGained = await PointConfiguration(merchant, body)
         await MerchantEnd(req, res, merchant, user, { point: pointGained, ...body })
@@ -22,12 +22,14 @@ exports.ApiController = async (req, res) => {
 const PointConfiguration = async (merchant, body) => {
     const data = await MerchantPointConfig.findOne({ where: { merchant_id: merchant.id } })
     const point = (parseFloat(body.TA) * parseFloat(data.value)) / parseFloat(data.amounts)
-    return point
+    return point.toFixed(2)
 
 }
 
 const MerchantEnd = async (req, res, merchant, user, body) => {
+    console.log(merchant?.User?.phone)
     await Points.findOne({ where: { user_id: merchant?.User?.phone } }).then(async (e) => {
+        console.log(e.points)
         if (parseFloat(e.points) < parseFloat(body.point)) {
             return validationError(res, "Your point is not sufficient.")
         }
@@ -41,16 +43,15 @@ const MerchantEnd = async (req, res, merchant, user, body) => {
             "others": `You sent ${body.point} points to ${body.UP} for purchasing of amount ${body.TA}.`,
         }
         await userPointTransfer(req, res, values)
-        await UserEnd(req, res, merchant, user, body).then(async () => {
-            return dataSuccess(res, "Done")
-        })
+        await UserEnd(req, res, merchant, user, body)
     })
 
 
 }
 
 const UserEnd = async (req, res, merchant, user, body) => {
-    await Points.findOne({ where: { user_id: user[0].phone } }).then(async (e) => {
+    await Points.findOrCreate({ where: { user_id: user[0].phone }, defaults: { user_id: user[0].phone, id: generateId() } }).then(async (data) => {
+        var e = data[0]
         e.update({ points: (parseFloat(e.points) + parseFloat(body.point)) })
         const values = {
             "token": null,
@@ -60,6 +61,8 @@ const UserEnd = async (req, res, merchant, user, body) => {
             "remarks": body.RE,
             "others": `You received ${body.point} points from ${merchant.parent_company} for purchasing of amount ${body.TA}.`,
         }
-        await userPointTransfer(req, res, values)
+        await userPointTransfer(req, res, values).then(() => {
+            dataSuccess(res, "Done")
+        })
     })
 }
